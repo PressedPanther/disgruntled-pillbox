9c3fc66f998766d958153bd9ecba7891a3cce603
PressedPanther <you@example.com>
2021-07-01 22:52:53 -0400

    first push
    +Everything
    >Homepage with video
    >Stream not working yet
    >Showcase not working yet, single vid box
    +FlaskSocketIO

---
+++ TCPClient.py
@@ -0,0 +1,14 @@
+#TCPClient.py
+
+from socket import socket, AF_INET, SOCK_STREAM
+
+serverName = 'localhost'
+#host = socket.gethostname()
+serverPort = 25565
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect((serverName, serverPort))
+message = input('Input lowercase sentence: ')
+clientSocket.send(message.encode())
+modifiedMessage = clientSocket.recv(2048).decode()
+print ('From Server: ', modifiedMessage)
+clientSocket.close()
---
+++ app/hello.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/showcase/')
+def showcase():
+    return render_template('showcase.html')
+
+@app.route('/stream/')
+def stream():
+    return render_template('stream.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
