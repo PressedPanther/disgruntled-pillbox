0bfe0a6fd6358a3106edd4ebdc734d0d55fbe687
PressedPanther <you@example.com>
2021-07-02 02:16:06 -0400

    Update hello.py

--- app/hello.py
+++ app/hello.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template
-app = Flask(__name__)
+app = Flask(__name__,static_folder=os.path.abspath(".../build"),static_url_path='/')
 
-@app.route('/')
+@app.route('/', defaults={'path':''})
 def home():
     return render_template('home.html')
+
+@app.route('/',defaults={'path':''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return app.send_static_file('home.html')
 
 @app.route('/showcase/')
 def showcase():
