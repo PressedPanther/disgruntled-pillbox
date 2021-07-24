15ca0d58aa9978dda1856c08c7e5cc7a3d103f0b
PressedPanther <you@example.com>
2021-07-24 16:53:48 -0400

    send helop
    broke livestream

--- app/hello.py
+++ app/hello.py
@@ -1,26 +1,26 @@
+from os import environ
 from pyimagesearch.motion_detection import singlemotiondetector
 from imutils.video import VideoStream
 from flask import Response
 from flask import (Flask,render_template,Response,request,redirect, session)
 #from flask_dance.contrib.github import make_github_blueprint, github
 #from werkzeug.contrib.fixers import ProxyFix
-from dotenv import load_dotenv
+import os
 import threading
 import argparse
 import datetime
 import imutils
 import time
 import cv2
-#import shh.env
 
 #outputFrame = None
 #lock = threading.Lock()
-load_dotenv()
 app = Flask(__name__)
 app.secret_key = os.getenv("client_secret")
 user = {"username": "abc", "password": "xyz"}
 video = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-@app.route('login',methods = ['POST','GET'])
+
+@app.route('/',methods = ['POST','GET'])
 def login():
     if(request.method == 'POST'):
         username = request.form.get('username')
@@ -37,11 +37,16 @@ def login():
   #  client_secret = os.environ.get(client_secret),
 #)
 #app.register_blueprint(blueprint, url_prefix="/login")
-#vs = VideoStream(src=0).start()
 
-#time.sleep(2.0)
 
-@app.route('/')
+
+#@app.route('/',defaults={'path':''})
+#@app.route('/<path:path>')
+#def catch_all(path):
+#    return app.send_static_file('home.html')
+
+
+@app.route('/home')
 def home():
     #if not github.authorized:
      #   return redirect(url_for("github.login"))
@@ -49,45 +54,9 @@ def home():
     #assert resp.ok
     #return "You are @{login} on GitHub".format(login=resp.json()["login"]))
     return render_template('home.html')
-#@app.route('/',defaults={'path':''})
-#@app.route('/<path:path>')
-#def catch_all(path):
-#    return app.send_static_file('home.html')
-
-@app.route('/showcase/')
-def showcase():
-    return render_template('showcase.html')
-
 @app.route('/stream/')
 def stream():
     return render_template('stream.html')
-
-#def detect_motion(frameCount):
-    global vs, outputFrame, lock
-    md = singlemotiondetector(accumWeight=0.1)
-    total = 0
-    while True:
-        frame = vs.read()
-        frame = imutils.resize(frame, width = 400)
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        gray = cv2.GaussianBlur(gray, (7,7), 0)
-
-        timestamp = datetime.datetime.now()
-        cv2.putText(frame,timestamp.strftime(
-           "%A %d %B %Y %I:%M:%S%p"), (10,frame.shape[0] - 10),
-           cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0,0,255), 1)
-        if total > frameCount:
-            motion = md.detect(gray)
-            if motion is not None:
-                (thresh, (minX, minY, maxX, maxY)) = motion
-                cv2.rectangle(frame, (minX,minY), (maxX, maxY),
-                (0,0,255), 2)
-                
-        md.update(gray)
-        total += 1
-
-        with lock: 
-            outputFrame = frame.copy()
 
 def gen(video):
     while True:
@@ -96,42 +65,11 @@ def gen(video):
         frame = jpeg.tobytes()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-#def generate():
-   #     global outputFrame, lock
-    #    while True:
-     #       with lock:
-      #          if outputFrame is None:
-       #             continue
-        #        (flag, encodedImage) = cv2.imencode(".jpg", outputFrame)
-#
- #               if not flag:
-  #                  continue
-   #          yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n'+ 
-    #             bytearray(encodedImage) + b'r\n')
 
 @app.route("/video_feed")
 def video_feed():
         return Response(gen(video),
         mimetype= "multipart/x-mixed-replace; boundary=frame")
 
-#if __name__ == '__main__':
- #   ap = argparse.ArgumentParser()
-  #  ap.add_argument("-i", "--ip", type=str, required=True,
-  #      help="ip address of the device")
-  #  ap.add_argument("-o", "--port", type=int,required=True,
-  #      help= "ephemeral port number of the server (1024 to 65535)")
-  #  ap.add_argument("-f","--frame-count", type=int, default=32,
-  #      help = "# of frames used to construct the background model")
-  #  args = vars(ap.parse_args())
-
-   # t = threading.Thread(target=detect_motion,args=(
-   #     args["frame_count"],
-   # ))
-   # t.daemon = True
-   # t.start()
-   # app.run(host=args["ip"], port=args["port"], debug=True, threaded=True,use_reloader=False)
-
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000,threaded=True)
-
-#vs.stop()
